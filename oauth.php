7d9abef30d194e25a516282580c506d61558b6a3
Andrew Rose <rose.andrew@gmail.com>
2019-05-23 09:40:26 +0100

    Update oauth.php
    Set CURLOPT_SSL_VERIFYPEER to true!

--- oauth.php
+++ oauth.php
@@ -10,7 +10,7 @@ if(isset($_GET['code']) && isset($_GET['state']) && in_array($_GET['state'], ['m
 	curl_setopt($ch, CURLOPT_URL, $settings[$idp]['endpointToken']);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, ['client_id' => $settings[$idp]['clientId'], 'client_secret' => $settings[$idp]['clientSecret'], 'redirect_uri' => $settings['default']['redirectUri'], 'code' => $_GET['code'], 'grant_type' => 'authorization_code', 'scope' => $settings[$idp]['scope']]);
 	$data = json_decode(curl_exec($ch), true);
 	if(!$data || !isset($data['access_token']))
@@ -22,7 +22,7 @@ if(isset($_GET['code']) && isset($_GET['state']) && in_array($_GET['state'], ['m
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $settings[$idp]['endpointScope']);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$data['access_token']));
 	$data = json_decode(curl_exec($ch), true);
 
